802dfc92fac016e802a91d3324285903a4bd2c7d
lgersman <lars.gersmann@gmail.com>
2012-02-08 11:48:09 +0100

    with statement removed to be compatible with google closure compiler

--- jquery.orangevolt-parse-jse.js
+++ jquery.orangevolt-parse-jse.js
@@ -25,220 +25,218 @@
 	/**
 	 * javascript expression grammar
 	 */ 
-	with( $.orangevolt.parse) {
-		$.orangevolt.parse.jse.grammar = Grammar({
-			lexer : {
-				skip   : /((\s*)|(\/{2}.*(\r\n|\r|\n))|(\/\*((\*[^\/])|.)*\*\/))*/, 
-				tokens : {
-					IDENTIFIER  : /[a-zA-Z_]\w*/,	
-					NUMBER      : /\d+(\.\d+)?/,	
-					'||'		: /\|\|/,
-					'|'			: /\|/,
-					'&&'		: /\&\&/,
-					'&'			: /\&/,
-					'==='		: /===/,
-					'!=='		: /!==/,
-					'=='		: /==/,
-					'!='		: /!=/,
-					'++'       	: /\+\+/,
-					'--' 	    : /\-\-/,
-					'+'        	: /\+/,
-					'-' 	    : /\-/,
-					'*'	    	: /\*/,
-					'/'     	: /\//,
-					'>='       	: /\>=/,
-					'<=' 	    : /\<=/,
-					'>'	    	: /\>/,
-					'<'     	: /\</,
-					'%'     	: /\%/,
-					':'     	: /\:/,
-					'.'     	: /\./,
-					'='     	: /\=/,
-					'?'     	: /\?/,
-					'('		  	: /\(/,
-					')'			: /\)/,
-					','			: /\,/,						
-					'INSTANCEOF': /instanceof/,
-					STRING		: /('((\\')|[^'])*')|("((\\")|[^"])*")/,
-					'['			: /\[/,
-					']'			: /\]/,
-					'{'			: /\{/,
-					'}'			: /\}/						
-				}
-			},
-		 	rules : [
-		 		Once( 'expr',
-				 	Term( 'cond-expr') 	
-				),
-				Once( 'cond-expr',
-				 	Term( 'cond-or-expr').Optional( 'cond-or-expr-right',
-						Token('?').Term( 'expr').Token(':').Term('expr') 
-					) 	
-				),
-				Once( 'cond-or-expr',
-					Term('cond-and-expr').ZeroOrMore( 'cond-and-expr-right',
-						Token('||').Term('cond-and-expr')
-					)
-				),
-				Once( 'cond-and-expr',
-					Term('incl-or-expr').ZeroOrMore( 'cond-and-expr-right',
-						Token('&&').Term('incl-or-expr')
-					)
-				),
-				Once( 'incl-or-expr',
-					Term('excl-or-expr').ZeroOrMore( 'incl-or-expr-right',
-						Token('|').Term('excl-or-expr')
-					)
-				),
-				Once( 'excl-or-expr',
-					Term('and-expr').ZeroOrMore( 'excl-or-expr-right',
-						Token('^').Term('and-expr')
-					)
-				),
-				Once( 'and-expr',
-					Term('ident-expr').ZeroOrMore( 'and-expr-right',
-						Token('&').Term('ident-expr')
-					)
-				),
-				Once( 'ident-expr',
-					Term('equ-expr').ZeroOrMore( 'ident-expr-right',
-						OneOf( 'ident-operator', 
-							Token('!=='),
-							Token('===')
-						)
-						.Term('equ-expr')
-					)
-				),
-				Once( 'equ-expr',
-					Term('instanceof-expr').ZeroOrMore( 'equ-expr-right',
-						OneOf( 'equ-operator', 
-							Token('!='),
-							Token('==')
-						)
-						.Term('instanceof')
-					)
-				),
-				Once( 'instanceof-expr',
-					Term('rel-expr').Optional( 'instanceof-expr-right',
-						Token( 'INSTANCEOF').Token( 'IDENTIFIER')
-					)
-				),
-				Once( 'rel-expr',
-					Term('add-expr').ZeroOrMore( 'rel-expr-right',
-						OneOf( 'rel-operator', 
-							Token('<'),
-							Token('>'),
-							Token('<='),
-							Token('>=')
-						)
-						.Term('add-expr')
-					)
-				),
-				Once( 'add-expr',
-					Term('mul-expr').ZeroOrMore( 'add-expr-right',
-						OneOf( 'add-operator', 
-							Token('+'),
-							Token('-')
-						)
-						.Term('mul-expr')
-					)
-				),
-				Once( 'mul-expr',
-					Term('unary-expr').ZeroOrMore( 'mul-expr-right',
-						OneOf( 'mul-operator', 
-							Token('*'),
-							Token('/'),
-							Token('%')
-						)
-						.Term('unary-expr')
-					)
-				),
-				OneOf( 'unary-expr',
-					OneOf( 'unary-operator', 
-						Token( '+'), Token( '-')
-					).Term( 'unary-expr'),
-					Term('pre-inc-expr'),	
-					Term('pre-dec-expr'),
-					Term('neg-expr') 	
-				),
-				Once( 'pre-inc-expr',
-					Token('++').Term( 'prim-expr')
-				),
-				Once( 'pre-dec-expr',
-					Token('--').Term( 'prim-expr')
-				),
-				OneOf( 'neg-expr',
-					Token('!').Term( 'unary-expr'),
-					Term('postfix-expr')	
-				),
-				Once( 'postfix-expr',
-					Term( 'prim-expr').Optional( 'postfix-expr-right',
-						OneOf( 'postfix-operator', Token( '++'), Token( '--'))
-					)	
-				),
-				Once( 'prim-expr',
-					Term( 'prim-prefix').ZeroOrMore( 'prim-expr-right',
-						Term('prim-suffix')
-					)
-				),
-				OneOf( 'prim-prefix',
-					Term('literal'),
-					Token('(').Term( 'expr').Token(')'),
-					Term( 'array'),
-					Term( 'object')
-				),
-				OneOf( 'prim-suffix',
-					Term( 'member'),
-					Term( 'array'),
-					Term( 'args')
-				),
-				Once( 'member',
-					Token('.').Token( 'IDENTIFIER')
-				),
-				Once( 'array',
-					Token('[').Optional( 'params', 
-						Term('arg-list')
-					).Token( ']')
-				),
-				Once( 'object',
-					Token('{').ZeroOrMore( 'object-expr-right',
-						Term('object-properties')
-					).Token( '}')
-				),
-				Once( 'object-properties',
-					Term( 'object-property').ZeroOrMore( 'object-properties-right',
-						Token(',').Term( 'object-property')
-					)
-				),
-				Once( 'object-property',
-					/*	
-					OneOf( 'property',
-						Token( 'IDENTIFIER'),
-						Token( 'STRING')
-					)
-					*/
-					Term( 'literal')	
-					.Token( ':')
-					.Term( 'expr')
-				),
-				OneOf( 'literal',
-					Token('NUMBER'),
-					Token('STRING'),
-					Token('IDENTIFIER')
-				),
-				Once( 'args',
-					Token('(').Optional( 'params', 
-						Term('arg-list')
-					).Token( ')')
-				),
-				Once( 'arg-list',
-					Term( 'expr').ZeroOrMore( 'arg-list-right',
-						Token(',').Term( 'expr')
-					)
+	$.orangevolt.parse.jse.grammar = $.orangevolt.parse.Grammar({
+		lexer : {
+			skip   : /((\s*)|(\/{2}.*(\r\n|\r|\n))|(\/\*((\*[^\/])|.)*\*\/))*/, 
+			tokens : {
+				IDENTIFIER  : /[a-zA-Z_]\w*/,	
+				NUMBER      : /\d+(\.\d+)?/,	
+				'||'		: /\|\|/,
+				'|'			: /\|/,
+				'&&'		: /\&\&/,
+				'&'			: /\&/,
+				'==='		: /===/,
+				'!=='		: /!==/,
+				'=='		: /==/,
+				'!='		: /!=/,
+				'++'       	: /\+\+/,
+				'--' 	    : /\-\-/,
+				'+'        	: /\+/,
+				'-' 	    : /\-/,
+				'*'	    	: /\*/,
+				'/'     	: /\//,
+				'>='       	: /\>=/,
+				'<=' 	    : /\<=/,
+				'>'	    	: /\>/,
+				'<'     	: /\</,
+				'%'     	: /\%/,
+				':'     	: /\:/,
+				'.'     	: /\./,
+				'='     	: /\=/,
+				'?'     	: /\?/,
+				'('		  	: /\(/,
+				')'			: /\)/,
+				','			: /\,/,						
+				'INSTANCEOF': /instanceof/,
+				STRING		: /('((\\')|[^'])*')|("((\\")|[^"])*")/,
+				'['			: /\[/,
+				']'			: /\]/,
+				'{'			: /\{/,
+				'}'			: /\}/						
+			}
+		},
+	 	rules : [
+	 		$.orangevolt.parse.Once( 'expr',
+	 				$.orangevolt.parse.Term( 'cond-expr') 	
+			),
+			$.orangevolt.parse.Once( 'cond-expr',
+					$.orangevolt.parse.Term( 'cond-or-expr').Optional( 'cond-or-expr-right',
+							$.orangevolt.parse.Token('?').Term( 'expr').Token(':').Term('expr') 
+				) 	
+			),
+			$.orangevolt.parse.Once( 'cond-or-expr',
+					$.orangevolt.parse.Term('cond-and-expr').ZeroOrMore( 'cond-and-expr-right',
+							$.orangevolt.parse.Token('||').Term('cond-and-expr')
 				)
-		 	]
-		}); 
-	}
+			),
+			$.orangevolt.parse.Once( 'cond-and-expr',
+					$.orangevolt.parse.Term('incl-or-expr').ZeroOrMore( 'cond-and-expr-right',
+							$.orangevolt.parse.Token('&&').Term('incl-or-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'incl-or-expr',
+					$.orangevolt.parse.Term('excl-or-expr').ZeroOrMore( 'incl-or-expr-right',
+							$.orangevolt.parse.Token('|').Term('excl-or-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'excl-or-expr',
+					$.orangevolt.parse.Term('and-expr').ZeroOrMore( 'excl-or-expr-right',
+							$.orangevolt.parse.Token('^').Term('and-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'and-expr',
+					$.orangevolt.parse.Term('ident-expr').ZeroOrMore( 'and-expr-right',
+							$.orangevolt.parse.Token('&').Term('ident-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'ident-expr',
+					$.orangevolt.parse.Term('equ-expr').ZeroOrMore( 'ident-expr-right',
+							$.orangevolt.parse.OneOf( 'ident-operator', 
+									$.orangevolt.parse.Token('!=='),
+									$.orangevolt.parse.Token('===')
+					)
+					.Term('equ-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'equ-expr',
+					$.orangevolt.parse.Term('instanceof-expr').ZeroOrMore( 'equ-expr-right',
+							$.orangevolt.parse.OneOf( 'equ-operator', 
+									$.orangevolt.parse.Token('!='),
+									$.orangevolt.parse.Token('==')
+					)
+					.Term('instanceof')
+				)
+			),
+			$.orangevolt.parse.Once( 'instanceof-expr',
+					$.orangevolt.parse.Term('rel-expr').Optional( 'instanceof-expr-right',
+							$.orangevolt.parse.Token( 'INSTANCEOF').Token( 'IDENTIFIER')
+				)
+			),
+			$.orangevolt.parse.Once( 'rel-expr',
+					$.orangevolt.parse.Term('add-expr').ZeroOrMore( 'rel-expr-right',
+							$.orangevolt.parse.OneOf( 'rel-operator', 
+									$.orangevolt.parse.Token('<'),
+									$.orangevolt.parse.Token('>'),
+									$.orangevolt.parse.Token('<='),
+									$.orangevolt.parse.Token('>=')
+					)
+					.Term('add-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'add-expr',
+					$.orangevolt.parse.Term('mul-expr').ZeroOrMore( 'add-expr-right',
+							$.orangevolt.parse.OneOf( 'add-operator', 
+									$.orangevolt.parse.Token('+'),
+									$.orangevolt.parse.Token('-')
+					)
+					.Term('mul-expr')
+				)
+			),
+			$.orangevolt.parse.Once( 'mul-expr',
+					$.orangevolt.parse.Term('unary-expr').ZeroOrMore( 'mul-expr-right',
+							$.orangevolt.parse.OneOf( 'mul-operator', 
+									$.orangevolt.parse.Token('*'),
+									$.orangevolt.parse.Token('/'),
+									$.orangevolt.parse.Token('%')
+					)
+					.Term('unary-expr')
+				)
+			),
+			$.orangevolt.parse.OneOf( 'unary-expr',
+					$.orangevolt.parse.OneOf( 'unary-operator', 
+							$.orangevolt.parse.Token( '+'), $.orangevolt.parse.Token( '-')
+				).Term( 'unary-expr'),
+				$.orangevolt.parse.Term('pre-inc-expr'),	
+				$.orangevolt.parse.Term('pre-dec-expr'),
+				$.orangevolt.parse.Term('neg-expr') 	
+			),
+			$.orangevolt.parse.Once( 'pre-inc-expr',
+					$.orangevolt.parse.Token('++').Term( 'prim-expr')
+			),
+			$.orangevolt.parse.Once( 'pre-dec-expr',
+					$.orangevolt.parse.Token('--').Term( 'prim-expr')
+			),
+			$.orangevolt.parse.OneOf( 'neg-expr',
+					$.orangevolt.parse.Token('!').Term( 'unary-expr'),
+					$.orangevolt.parse.Term('postfix-expr')	
+			),
+			$.orangevolt.parse.Once( 'postfix-expr',
+					$.orangevolt.parse.Term( 'prim-expr').Optional( 'postfix-expr-right',
+							$.orangevolt.parse.OneOf( 'postfix-operator', $.orangevolt.parse.Token( '++'), $.orangevolt.parse.Token( '--'))
+				)	
+			),
+			$.orangevolt.parse.Once( 'prim-expr',
+					$.orangevolt.parse.Term( 'prim-prefix').ZeroOrMore( 'prim-expr-right',
+							$.orangevolt.parse.Term('prim-suffix')
+				)
+			),
+			$.orangevolt.parse.OneOf( 'prim-prefix',
+					$.orangevolt.parse.Term('literal'),
+					$.orangevolt.parse.Token('(').Term( 'expr').Token(')'),
+					$.orangevolt.parse.Term( 'array'),
+					$.orangevolt.parse.Term( 'object')
+			),
+			$.orangevolt.parse.OneOf( 'prim-suffix',
+					$.orangevolt.parse.Term( 'member'),
+					$.orangevolt.parse.Term( 'array'),
+					$.orangevolt.parse.Term( 'args')
+			),
+			$.orangevolt.parse.Once( 'member',
+					$.orangevolt.parse.Token('.').Token( 'IDENTIFIER')
+			),
+			$.orangevolt.parse.Once( 'array',
+					$.orangevolt.parse.Token('[').Optional( 'params', 
+							$.orangevolt.parse.Term('arg-list')
+				).Token( ']')
+			),
+			$.orangevolt.parse.Once( 'object',
+					$.orangevolt.parse.Token('{').ZeroOrMore( 'object-expr-right',
+							$.orangevolt.parse.Term('object-properties')
+				).Token( '}')
+			),
+			$.orangevolt.parse.Once( 'object-properties',
+					$.orangevolt.parse.Term( 'object-property').ZeroOrMore( 'object-properties-right',
+							$.orangevolt.parse.Token(',').Term( 'object-property')
+				)
+			),
+			$.orangevolt.parse.Once( 'object-property',
+				/*	
+				OneOf( 'property',
+					Token( 'IDENTIFIER'),
+					Token( 'STRING')
+				)
+				*/
+					$.orangevolt.parse.Term( 'literal')	
+				.Token( ':')
+				.Term( 'expr')
+			),
+			$.orangevolt.parse.OneOf( 'literal',
+					$.orangevolt.parse.Token('NUMBER'),
+					$.orangevolt.parse.Token('STRING'),
+					$.orangevolt.parse.Token('IDENTIFIER')
+			),
+			$.orangevolt.parse.Once( 'args',
+					$.orangevolt.parse.Token('(').Optional( 'params', 
+							$.orangevolt.parse.Term('arg-list')
+				).Token( ')')
+			),
+			$.orangevolt.parse.Once( 'arg-list',
+					$.orangevolt.parse.Term( 'expr').ZeroOrMore( 'arg-list-right',
+							$.orangevolt.parse.Token(',').Term( 'expr')
+				)
+			)
+	 	]
+	}); 
 	
 	/**
 	 * purge function usable as argument for function $.orangevolt.parse.AST.purge( ...)
